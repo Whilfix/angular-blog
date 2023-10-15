4f0fd40c0a61abccc73eb8ddb9566086f7ae56b1
Willian Orth <willian.orth@gmail.com>
2023-10-15 16:22:46 -0300

    finished general structure

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "link",
+        "title": "titulo1",
+        "description": "ola1"
+    },
+    {
+        "id": "2",
+        "photo": "link",
+        "title": "titulo2",
+        "description": "ola2"
+    },
+    {
+        "id": "3",
+        "photo": "link",
+        "title": "titulo3",
+        "description": "ola3"
+    },
+    {
+        "id": "4",
+        "photo": "link",
+        "title": "titulo4",
+        "description": "ola4"
+    }
+]
